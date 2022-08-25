ecdc87d548d9956526e531ce127b1955cc0864d0
Aljhon21 <40093517+Aljhon21@users.noreply.github.com>
2022-08-25 22:47:49 +0800

    install and setup tailwind

--- resources/js/src/main.js
+++ resources/js/src/main.js
@@ -3,7 +3,6 @@ import { createApp } from 'vue';
 import router from '@/router/routes.js';
 import App from './App.vue';
 
-
  const app = createApp(App);
  app.use(router);
  app.mount('#app');
--- resources/js/src/pages/Home.vue
+++ resources/js/src/pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
-        <h1>HI</h1>
-    </div>
+  <h1 class="text-4xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
